4edade50c0c84bf3e0eeb53d2ea1082b526b7067
Domingo Velazquez <domingoandresv98@gmail.com>
2025-04-26 11:22:24 -0400

    Primer commit

---
+++ src/main/java/com/example/hospital_mv/HospitalMvApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospital_mv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalMvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalMvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hospital_mv/HospitalMvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospital_mv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalMvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
